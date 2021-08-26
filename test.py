f90075be11f4bf8268ce776e1a0c10057a6d1978
Wiratama <dafawiratama13@gmail.com>
2021-08-26 09:22:12 +0700

    New Approach

--- test.py
+++ test.py
@@ -1,57 +1,89 @@
 import os
 
+import matplotlib.pyplot as plt
 import pandas as pd
-from tensorflow.python.keras.layers import LSTM, Dropout, Dense
-from tensorflow.python.keras.models import Sequential
-from tqdm import tqdm
+import numpy as np
 import tensorflow as tf
-
-from WindowGenerator import WindowGenerator
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-
-def get_dataset(dataset):
-    print("Loading Dataset")
-    frame = []
-    for state in tqdm(os.listdir(f"dataset/{dataset}")):
-        label = encode_state(state)
-        for folder in os.listdir(f"dataset/{dataset}/{state}"):
-            dataframe = pd.read_csv(f"dataset/{dataset}/{state}/{folder}")
-            dataframe = dataframe.drop(dataframe.columns[0], axis=1)
-            dataframe["state"] = label
-            frame.append(dataframe)
-    return pd.concat(frame)
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import LSTM, Dropout, Dense, ConvLSTM2D, Flatten
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
 
-def encode_state(state):
-    if "dws" in state:
-        return 0
-    elif "jog" in state:
-        return 1
-    elif "sit" in state:
-        return 2
-    elif "std" in state:
-        return 3
-    elif "ups" in state:
-        return 4
-    elif "wlk" in state:
-        return 5
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def create_window(dataframe, window_size=32, stride=1):
+    data = np.array(dataframe)
+    dataframe_size = len(dataframe)
+    feature_size = dataframe.values.shape[1] - 1
+    dataset_x = data[:, :-1]
+    dataset_y = data[:, -1]
+
+    windows_x = np.empty(shape=(int(dataframe_size / stride), window_size, feature_size))
+    windows_y = np.empty(shape=(int(dataframe_size / stride), 1))
+
+    for offset in range(0, dataframe_size - window_size, stride):
+        indices = np.asarray([i for i in range(window_size)]) + offset
+        windows_x[int(offset / stride)] = dataset_x[indices]
+        windows_y[int(offset / stride)] = dataset_y[offset + window_size - 1]
+
+    return windows_x, to_categorical(windows_y)
+
+
+STATE = {
+    "dws": 0,
+    "jog": 1,
+    "sit": 2,
+    "std": 3,
+    "ups": 4,
+    "wlk": 5,
+}
+
+
+def dataset_motion_sense(type="Device Motion"):
+    list = []
+    for folder in os.listdir(f"dataset/Motion Sense/{type}"):
+        state = STATE.get(folder[:3])
+        for file in os.listdir(f"dataset/Motion Sense/{type}/{folder}"):
+            df = pd.read_csv(f"dataset/Motion Sense/{type}/{folder}/{file}")
+            df.pop(df.columns[0])
+            df["state"] = state
+            list.append(df)
+    return pd.concat(list)
 
 
 if __name__ == '__main__':
-    dataframe = get_dataset("Device Motion")
-    n = len(dataframe)
-    train_df = dataframe[0:int(n * 0.7)]
-    val_df = dataframe[int(n * 0.7):int(n * 0.9)]
-    test_df = dataframe[int(n * 0.9):]
-    window = WindowGenerator(input_width=32, label_width=1, shift=1, label_columns=['state'], train_df=train_df, val_df=val_df, test_df=test_df)
+    dataframe = dataset_motion_sense("Accelerometer")
+
+    print(np.concatenate((a.values, b.values), axis=-1).shape)
+    print(a.head())
+    print(b.head())
+    print(dataframe.head())
+    exit()
+    x, y = create_window(dataframe, window_size=256, stride=32)
+
+    n_features, n_outputs = x.shape[2], x.shape[1]
+    n_steps, n_length = 4, 64
+    x = x.reshape((x.shape[0], n_steps, 1, n_length, n_features))
+
+    size = len(x)
+    train_x = x[:round(size * .8)]
+    train_y = y[:round(size * .8)]
+    val_x = x[round(size * .7):round(size * .9)]
+    val_y = y[round(size * .7):round(size * .9)]
+    test_x = x[round(size * .9):]
+    test_y = y[round(size * .9):]
 
     model = Sequential()
-    model.add(LSTM(128, input_shape=(32, 13)))
-    model.add(Dropout(0.5))
-    model.add(Dense(128, activation='relu'))
+    model.add(ConvLSTM2D(filters=128, kernel_size=(1, 3), input_shape=(n_steps, 1, n_length, n_features)))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(6, activation='softmax'))
-    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit network
+    model.fit(x=train_x, y=train_y, validation_data=(val_x, val_y), epochs=16, batch_size=1024)
 
-    model.fit(window.train, validation_data=window.val, epochs=8, batch_size=1024)
+    model.evaluate(test_x, test_y)
